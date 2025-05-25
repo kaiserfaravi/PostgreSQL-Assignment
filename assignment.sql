7f90a5a19517fce49703491e6129b418736b2580
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-05-26 03:24:38 +0600

    replaced

--- assignment.sql
+++ assignment.sql
@@ -1,12 +1,50 @@
 -- Active: 1747640292108@@localhost@5432@conservation_db
-CREATE Table kaiser(
-    id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(50),
-    age int 
 
+----------------------------------------------------------- tables-------------------------------------------------------
+
+CREATE Table rangers(
+    ranger_id SERIAL NOT NULL PRIMARY Key,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(250) NOT NULL
 );
 
-INSERT INTO kaiser (name,age)
-VALUES ('kaiser',21)
+CREATE Table species(
+    species_id SERIAL NOT NULL PRIMARY Key,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR NOT NULL
+);
 
-SELECT * FROM kaiser;
+
+CREATE Table sightings(
+    sighting_id SERIAL NOT NULL PRIMARY Key,
+    ranger_id INTEGER REFERENCES rangers(ranger_id ),
+    species_id INTEGER REFERENCES species(species_id),
+    sighting_time DATE NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    notes VARCHAR(250)
+);
+
+INSERT INTO rangers (name,region) 
+values( 'Alice Green', 'Northern Hills'),
+( 'Bob White', 'River Delta'),
+('Carol King', 'Mountain Range')
+
+SELECT * FROM rangers;
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status)
+ values ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+SELECT * FROM species;
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * FROM sightings;
